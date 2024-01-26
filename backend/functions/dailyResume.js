fe819be64b2ec1556ffef6651258bf11b6e57687
MeekoDev <DiegoCru1024@hotmail.com>
2024-01-26 17:27:40 -0500

    Update

--- backend/functions/dailyResume.js
+++ backend/functions/dailyResume.js
@@ -1,7 +1,7 @@
 const {resultModel} = require("../models/resultModel");
 const {resumeModel} = require("../models/resumeModel");
 const uuid = require("uuid");
-const domains = ['prod-protuner.samishop.pe', 'prod-kawasaki.samishop.pe', 'prod-croslandstore.samishop.pe', 'prod-rok.samishop.pe'];
+const domains = ['protuner.pe', 'kawasaki.com.pe', 'croslandstore.com.pe', 'prod-rok.samishop.pe'];
 
 async function generateDailyResume() {
     for (const domain of domains) {
